d7d5b91ea51fefca7654a93afd88d09be44bb3f8
Ginto Philip <gintophilip.com@gmail.com>
2025-06-14 06:19:39 +0530

    cleanup

--- src/main/java/com/gintophilip/shipproxy/Main.java
+++ src/main/java/com/gintophilip/shipproxy/Main.java
@@ -4,8 +4,6 @@ import com.gintophilip.shipproxy.proxy.ShipProxy;
 
 import java.io.IOException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Integer listenPort = null;
